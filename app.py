39c4d82807cb86b81b05d2dbb919490e1a12f6bd
Adnan <144677372+stackadnan@users.noreply.github.com>
2025-05-29 14:25:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
-@app.route("/hello", methods=["GET"])
+@app.route('/hello', methods=['GET'])
 def hello_world():
     return jsonify({"message": "Hello, World!"})
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT provided by Render
+    app.run(host='0.0.0.0', port=port)        # Bind to 0.0.0.0 for public access
